28d297891940a01762f92f03329b5ebe14d1124a
hyong <hyong_cs@outlook.com>
2021-01-12 09:45:58 +0800

    Initial commit

---
+++ src/main/java/suep/rg/brcode/BrcodeApplication.java
@@ -0,0 +1,13 @@
+package suep.rg.brcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/suep/rg/brcode/BrcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package suep.rg.brcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
